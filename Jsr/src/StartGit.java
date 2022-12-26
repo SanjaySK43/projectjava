9ecae207deefde1bbd15d1bb457925530ee63d01
Test <ksanj43@gmail.com>
2022-12-26 09:56:22 +0530

    Added Addition

--- Jsr/src/StartGit.java
+++ Jsr/src/StartGit.java
@@ -2,6 +2,12 @@
 public class StartGit {
 
 	public static void main(String[] args) {
+		
+		int a=12;
+		int b=24;
+		int c=a+b;
+		
+		System.out.println(c);
 		System.out.println("Welcome to Git.....!");
 
 	}
